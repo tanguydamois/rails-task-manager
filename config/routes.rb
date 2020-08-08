58261ecbb2502728ce625138e4adbfd3fae4637a
tanguydamois <tdamois@gmail.com>
2020-08-08 15:10:11 -0400

    create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
   get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
 
 end
