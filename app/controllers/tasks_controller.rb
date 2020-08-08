ce4610c2d5822463f0bc2bf1b4395d78ba8a6626
tanguydamois <tdamois@gmail.com>
2020-08-08 13:48:19 -0400

    show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,8 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def
+    @tasks = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+
 end
